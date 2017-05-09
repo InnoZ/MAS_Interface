b05018e6851a222cf618871988c7806c3a9490e1
Benjamin Stolte <benjamin.stolte@innoz.de>
2017-05-09 13:01:50 +0200

    add matsim starter

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -7,5 +7,10 @@ class ScenariosController < ApplicationController
   def run
     flash[:success] = "Folgendes Szenario wurde gewählt: Jahr #{params[:year]} - GKZ #{params[:district_id]}"
     redirect_back(fallback_location: root_path)
+    start_matsim
+  end
+
+  def start_matsim
+    MatsimStarter.new(Integer(params[:district_id]), Integer(params[:year])).start
   end
 end
---
+++ app/models/matsim_starter.rb
@@ -0,0 +1,24 @@
+class MatsimStarter
+  DEFAULT = Rails.root.join('public', 'matsim', 'xml').to_s
+  MATSIM = Rails.root.join('lib', 'matsim', 'jar').to_s
+
+  def initialize(id, year, folder = DEFAULT)
+    @id = id
+    @year = year
+    @folder = folder
+    raise "#{year} must be between 2009 and 2040" unless year_range.include?(year)
+  end
+
+  # rubocop:disable LineLength
+  def start
+    Kernel.system "java -cp #{MATSIM}/innoz-toolbox-0.1-SNAPSHOT.jar com.innoz.toolbox.run.Preto #{id} #{year} #{folder} >/dev/null 2>&1"
+  end
+
+  private
+
+  def year_range
+    2009..2040
+  end
+
+  attr_reader :id, :year, :folder
+end
