10eeb8f659e3efb05d65eb43167cbc2a6d6c69ce
kjoscha <kjoscha@gmail.com>
2017-05-09 16:10:17 +0200

    scope javascript

--- app/assets/javascripts/scenarios/select.js
+++ app/assets/javascripts/scenarios/select.js
@@ -1,44 +1,46 @@
 jQuery(function() {
-  map = L.map('map-germany', {
-    maxZoom: 8,
-    minZoom: 6,
-  });
+  jQuery('#map-germany').each(function() {
+    map = L.map('map-germany', {
+      maxZoom: 8,
+      minZoom: 6,
+    });
 
-  L.geoJson(window.germany_simple)
-    .addTo(map)
-    .setStyle({fillColor: '#337ab7', fillOpacity: 1, stroke: false})
+    L.geoJson(window.germany_simple)
+      .addTo(map)
+      .setStyle({fillColor: '#337ab7', fillOpacity: 1, stroke: false})
 
-  var onEachFeature = function (feature, layer) {
-    var name = feature.properties.name;
-    layer.setStyle({fillColor: '#506A85'});
-    function mouseover(e) {
-      layer.setStyle({fillOpacity: 0.2});
-      jQuery('.district-name').html(feature.properties.name);
+    var onEachFeature = function (feature, layer) {
+      var name = feature.properties.name;
+      layer.setStyle({fillColor: '#506A85'});
+      function mouseover(e) {
+        layer.setStyle({fillOpacity: 0.2});
+        jQuery('.district-name').html(feature.properties.name);
+      };
+      function mouseout(e) {
+        layer.setStyle({fillOpacity: 1});
+        jQuery('.district-name').html('');
+      };
+      function onclick(e) {
+        jQuery('#district_id').val(feature.properties.id);
+      };
+      layer.on('mouseout', mouseout);
+      layer.on('mouseover', mouseover);
+      layer.on('click', onclick);
     };
-    function mouseout(e) {
-      layer.setStyle({fillOpacity: 1});
-      jQuery('.district-name').html('');
-    };
-    function onclick(e) {
-      jQuery('#district_id').val(feature.properties.id);
-    };
-    layer.on('mouseout', mouseout);
-    layer.on('mouseover', mouseover);
-    layer.on('click', onclick);
-  };
 
-  var districts = L.geoJson(window.districts_germany, {onEachFeature: onEachFeature});
-  districts.addTo(map);
-  districts.setStyle({fillOpacity: 1, weight: 0.5, color: 'black'});
+    var districts = L.geoJson(window.districts_germany, {onEachFeature: onEachFeature});
+    districts.addTo(map);
+    districts.setStyle({fillOpacity: 1, weight: 0.5, color: 'black'});
 
-  map.fitBounds(districts.getBounds());
+    map.fitBounds(districts.getBounds());
 
-  jQuery(window).mousemove(function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    jQuery('.district-name').css({
-      'top': mouseY,
-      'left': mouseX,
+    jQuery(window).mousemove(function(e) {
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      jQuery('.district-name').css({
+        'top': mouseY,
+        'left': mouseX,
+      });
     });
   });
 });
