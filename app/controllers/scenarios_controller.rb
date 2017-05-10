2255b7b8febd77803e2e6f9944aad0c7fe54abc4
Benjamin Stolte <benjamin.stolte@innoz.de>
2017-05-10 14:41:01 +0200

    check for existing records
    else you can create infinite same models

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -13,22 +13,34 @@ class ScenariosController < ApplicationController
   end
 
   def create
-    @scenario = Scenario.new(
-      district_id: String(scenario_params[:district_id]),
-      year: scenario_params[:year],
-      json: result
-    )
-    if @scenario.save
-      flash[:success] = 'Szenario erstellt'
+    if record_exists?
+      flash[:success] = 'Szenario ist bereits vorhanden'
       redirect_to scenarios_path
+    else
+      @scenario = Scenario.new(
+        district_id: String(scenario_params[:district_id]),
+        year: scenario_params[:year],
+        json: result
+      )
+      if @scenario.save
+        flash[:success] = 'Szenario erstellt'
+        redirect_to scenarios_path
+      end
     end
-  rescue => e
-    flash[:danger] = "Ups, etwas ist schief gegangen => #{e.message}"
-    redirect_to :back
+    rescue => e
+      flash[:danger] = "Ups, etwas ist schief gegangen => #{e.message}"
+      redirect_to :back
   end
 
   def result
     MatsimStarter.new(String(scenario_params[:district_id]), Integer(scenario_params[:year])).result
+  end
+
+  def record_exists?
+    !!Scenario.where(
+      district_id: String(scenario_params[:district_id]),
+      year: scenario_params[:year],
+    ).exists?
   end
 
   def scenario_params
