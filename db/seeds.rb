83fa6eeae33cbb70717997abd5bb0abb985e4831
Benjamin Stolte <benjamin.stolte@innoz.de>
2017-06-07 12:43:11 +0200

    add file check

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,23 @@
 SEED_PATH = Rails.root.join('db', 'seeds')
 DISTRICT_IDS = %w[09180 03404]
 
-Kernel.system "psql mas_interface_development < #{SEED_PATH}/seeds.dump"
+if File.file?("#{SEED_PATH}/seeds.dump")
+  Kernel.system "psql mas_interface_development < #{SEED_PATH}/seeds.dump"
 
-DISTRICT_IDS.each do |district_id|
-  Scenario.create(
-    district_id: district_id,
-    year: 2017,
-    population: 20_000,
-    population_diff_2017: 5_000,
-    person_km: [ ['carsharing', '15'], ['car', '50'] ],
-    trips: [ ['car', '10'], ['carsharing', '15'] ],
-    diurnal_curve: [ ['bike', '10', '5'], ['ride', '14', '7'] ],
-    carbon_emissions: [ ['ride', '20'], ['car', '100'] ],
-    seed: true
-  )
-  if Scenario.find_by(district_id: district_id, seed: true)
-    GridFill.new(scenario: Scenario.find_by(district_id: district_id, seed: true), side_length: Grid.default_side_length).run
+  DISTRICT_IDS.each do |district_id|
+    Scenario.create(
+      district_id: district_id,
+      year: 2017,
+      population: 20_000,
+      population_diff_2017: 5_000,
+      person_km: [ ['carsharing', '15'], ['car', '50'] ],
+      trips: [ ['car', '10'], ['carsharing', '15'] ],
+      diurnal_curve: [ ['bike', '10', '5'], ['ride', '14', '7'] ],
+      carbon_emissions: [ ['ride', '20'], ['car', '100'] ],
+      seed: true
+    )
+    if Scenario.find_by(district_id: district_id, seed: true)
+      GridFill.new(scenario: Scenario.find_by(district_id: district_id, seed: true), side_length: Grid.default_side_length).run
+    end
   end
 end
