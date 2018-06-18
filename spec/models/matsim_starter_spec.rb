8cff598980cb33f4a9146d691dc16894801673b3
kjoscha <kjoscha@gmail.com>
2018-06-18 12:54:01 +0200

    fix specs

--- spec/models/matsim_starter_spec.rb
+++ spec/models/matsim_starter_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe MatsimStarter, type: :model do
   it 'creates a scenario', :hard_cleanup do
     expect(Scenario.all.count).to eq 0
     expect(Grid.all.count).to eq 0
-    described_class.new(district_id, year)
+    described_class.new(district_id, year, 'any scenario name')
     expect(Scenario.all.count).to eq 1
     expect(Grid.all.count).to eq 775
   end
@@ -17,6 +17,6 @@ RSpec.describe MatsimStarter, type: :model do
   # rubocop:disable LineLength
   it 'raises runtime error if matsim does not create scenario' do
     error = 'Matsim scenario creation not completed. User \'matsim\' existing? Look into matsim logfiles for more information.'
-    expect { described_class.new(nil, 2017) }.to raise_error(RuntimeError, error)
+    expect { described_class.new(nil, 2017, 'any scenario name') }.to raise_error(RuntimeError, error)
   end
 end
