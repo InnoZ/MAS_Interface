552b9f82f6da1f46e890fe3cfb182b5138690c79
kjoscha <kjoscha@gmail.com>
2018-06-20 12:42:33 +0200

    Added calibration call; atm deactivatd but can be activated via var

--- app/models/matsim_starter.rb
+++ app/models/matsim_starter.rb
@@ -20,6 +20,7 @@ class MatsimStarter
 
   # rubocop:disable LineLength
   def run_matsim
+    run_calibration = false
     if rails_env == 'production'
       java_class = 'Main'
     else
@@ -27,9 +28,13 @@ class MatsimStarter
       sleep(10) if rails_env == 'development'
       java_class = 'Preto'
     end
-    Kernel.system("java -mx4g -cp #{JAVA_PATH} com.innoz.toolbox.run.#{java_class} #{district_id} #{year} #{folder} #{rails_env} #{LOG_PATH} >/dev/null 2>&1")
-    if rails_env == 'production'
-      Kernel.system("java -mx4g -cp #{JAVA_PATH} com.innoz.toolbox.run.calibration.#{CALIBRATION_CLASS} #{folder}/#{district_id}_#{year}/config.xml.gz >/dev/null 2>&1")
+    log_path_exp = LOG_PATH.join("#{district_id}_#{year}")
+    Dir.mkdir(log_path_exp)    
+    Kernel.system("java -Xmx18G -XX:-UseGCOverheadLimit -cp #{JAVA_PATH} com.innoz.toolbox.run.#{java_class} #{district_id} #{year} #{folder} #{rails_env} #{LOG_PATH} 2> #{log_path_exp}/stderr.log ")
+    if run_calibration == true
+      # Params Config.xml, sampleFactor, scenarioName
+      #  sampleFactor -> 1 = 1000 Agents; 0.5 = 500 Agents
+      Kernel.system("java -mx4g -cp #{JAVA_PATH} com.innoz.toolbox.run.calibration.#{CALIBRATION_CLASS} #{folder}/#{district_id}_#{year}/config.xml.gz 0.5 #{district_id}_#{year} 2> #{log_path_exp}/calibration_run_shell.log") 
     end
     @scenario = Scenario.find_by(year: year, district_id: district_id)
   end
